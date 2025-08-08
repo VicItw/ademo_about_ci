3bc3267fd7a8311fc2e2080ebbdd2b7cdcfe3551
VicItw <149157538+VicItw@users.noreply.github.com>
2025-08-08 14:13:16 +0700

    Update Counter.test.tsx

--- src/components/features/homepage/Counter.test.tsx
+++ src/components/features/homepage/Counter.test.tsx
@@ -21,6 +21,8 @@ describe('useCounter', () => {
     const { result } = renderHook(() => useCounter());
     act(() => {
       result.current.setVal(5);
+    });
+    act(() => {
       result.current.increment();
     });
     expect(result.current.count).toBe(5);
